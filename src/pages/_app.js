6f5702e785c2fbb6e21638e3a3fb68d3d628c5b3
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-12-20 09:10:20 +0530

    Standard EsLint Configured

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,21 @@
-import { ApolloProvider } from "@apollo/client";
+import React from 'react'
+import { ApolloProvider } from '@apollo/client'
+import PropTypes from 'prop-types'
 
-import client from "../config/apollo-client";
+import client from '../config/apollo-client'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  );
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
