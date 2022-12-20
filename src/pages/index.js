31b7d2a1ce7b5668c6e0a28ddf52c626a30aafd5
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-12-20 10:33:25 +0530

    propTypes related changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import styles from '@styles/pages/Home.module.css'
 import client from '@configs/apollo-client'
 import { GET_MENU_TREE } from '@graphql/home/home.query'
 
 export default function Home (props) {
-
   const { menu } = props
 
   console.log('menu', menu)
@@ -17,14 +17,18 @@ export default function Home (props) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps () {
   const { data } = await client.query({
     query: GET_MENU_TREE
-  });
+  })
 
   return {
     props: {
       menu: data.getMenuTree.aResults
     }
   }
+}
+
+Home.propTypes = {
+  menu: PropTypes.array
 }
