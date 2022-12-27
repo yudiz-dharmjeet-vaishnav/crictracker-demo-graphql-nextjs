92b47032c40d8feddbcd861d20a38b6f6752d0ce
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-12-27 17:13:31 +0530

    _document added

---
+++ src/pages/_document.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document () {
+  return (
+    <Html>
+      <Head>
+        <title>CricTracker</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
