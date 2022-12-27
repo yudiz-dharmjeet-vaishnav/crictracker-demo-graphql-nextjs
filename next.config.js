5ee7c892b2a3f13617e7d04eacc45bf6cdc5c942
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-12-27 12:18:34 +0530

    First article display successfully

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'crictracker-admin-panel-local-dev-08032022.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import Image from 'next/image'
 import PropTypes from 'prop-types'
+import { BsCalendarEvent, BsStopwatch } from 'react-icons/bs'
 
 import styles from '@styles/components/ArticleList.module.scss'
 
 function ArticleList (props) {
   const { homePageArticleData } = props
+
+  const preSignedUrl = 'https://crictracker-admin-panel-local-dev-08032022.s3.ap-south-1.amazonaws.com/'
 
   const [data, setData] = useState([])
 
@@ -16,7 +20,28 @@ function ArticleList (props) {
 
   return (
     <div className={styles.articleList}>
-      <h1>ArticleList</h1>
+      {data.map((item, index) => (
+        <Fragment key={index}>
+          <h2 className={styles.articleCategory}>{item.sName}</h2>
+          <hr />
+          {item.aArticle.map((article) => (
+            <Fragment key={article._id}>
+              {article.sType === 'nBig' && (
+                <div className={styles.big_article}>
+                  <Image src={`${preSignedUrl}${article.oImg.sUrl}`} width={400} height={400} />
+                  <h2>{article.sSrtTitle}</h2>
+                  <div className={styles.date}>
+                    <BsCalendarEvent />
+                    <h4>{article.dPublishDisplayDate}</h4>
+                    <BsStopwatch />
+                    <h4>{article.nDuration} Min</h4>
+                  </div>
+                </div>
+              )}
+            </Fragment>
+          ))}
+        </Fragment>
+      ))}
     </div>
   )
 }
