03783b5c827991653dbe82b0d90d0c590b6dc733
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-12-27 15:26:03 +0530

    Dynamic Footer is all READY

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import styles from '@styles/components/Footer.module.scss'
+
 function Footer (props) {
   const { footerData } = props
 
-  console.log('footerData', footerData)
-
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div className={styles.footer_container}>
+        <div className={styles.footer_top}>
+          {footerData.map((item, index) => (
+            <div key={index} className={styles.footerItem}>
+              <h2>{item.eType}</h2>
+              <hr />
+              <ul>
+                {item.aResults.map((item) => (
+                  <li key={item._id}>{item.sTitle}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
